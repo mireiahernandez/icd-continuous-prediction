20f608901a0103c1824ded7ecb8c368ad83a53c2
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-06-18 15:09:39 +0100

    slurm gpu submission with 16 chunks

---
+++ slurm_16_chunks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL # required to send email notifcations
+#SBATCH --mail-user=mh1022@ic.ac.uk # required to send email notifcations - please replace <your_username> with your college login name or email address
+
+source /vol/bitbucket/mh1022/dl_cw_pyenv/bin/activate
+python main.py --num_chunks 16 --run_name "Run_test_twlan_16_chunks"
+TERM=vt100 # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
