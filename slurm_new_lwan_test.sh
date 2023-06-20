6b67f9df3400af90d7facc6bd639c57d9c784616
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-06-20 10:26:29 +0100

    slurm submission scripts

--- slurm_new_lwan_test.sh
+++ slurm_new_lwan_test.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=mh1022@ic.ac.uk # required to send email notifcations - please replace <your_username> with your college login name or email address
 
 source /vol/bitbucket/mh1022/dl_cw_pyenv/bin/activate
-python main.py --num_chunks 4 --run_name "Run_new_lwan_4_chunks"
+python main.py --num_chunks 16 --run_name "2006_1036_Run_temporal"
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
