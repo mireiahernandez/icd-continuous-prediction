be5f13516edad8a47436ccd333e187f036e87b7e
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-06-25 13:49:15 +0100

    added gitignore files

--- notemp_eval.sh
+++ notemp_eval.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=mh1022@ic.ac.uk # required to send email notifcations - please replace <your_username> with your college login name or email address
 
 source /vol/bitbucket/mh1022/dl_cw_pyenv/bin/activate
-python main.py --num_chunks 4 --run_name "2206_1200_Run_temporal_4"
+python main.py --num_chunks 4 --run_name "2206_1648_All_chunk_Run_temporal_4"
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
