2bb73b79b3ac04fad6f5bb01b4707e2694a47e53
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-06-25 13:47:03 +0100

    added multiple predictions at different cutoffs

--- training/trainer.py
+++ training/trainer.py
@@ -18,6 +18,7 @@ from tqdm import tqdm
 from torch.cuda.amp import GradScaler, autocast
 from evaluation.metrics import MyMetrics
 from evaluation.evaluate import evaluate
+import ipdb
 from transformers import (
     AutoModelForSequenceClassification,
     TrainingArguments,
@@ -61,6 +62,8 @@ class Trainer:
 
         for e in range(training_args['TOTAL_COMPLETED_EPOCHS'], epochs):
             hyps = []
+            hyps_temp ={'2d':[],'5d':[],'13d':[],'noDS':[]}
+            refs_temp ={'2d':[],'5d':[],'13d':[],'noDS':[]}
             refs = []
             for t, data in enumerate(tqdm(training_generator)):
                 labels = data["label"][0][: self.model.num_labels]
@@ -69,7 +72,7 @@ class Trainer:
                 seq_ids = data["seq_ids"][0]
                 category_ids = data["category_ids"][0]
                 note_end_chunk_ids = data["note_end_chunk_ids"]
-
+                cutoffs = data["cutoffs"]
                 with torch.cuda.amp.autocast(enabled=True) as autocast, torch.backends.cuda.sdp_kernel(enable_flash=False) as disable :
                 # with autocast():
                     scores = self.model(
@@ -86,9 +89,17 @@ class Trainer:
                     self.scaler.scale(loss).backward()
                     # convert to probabilities
                     probs = F.sigmoid(scores)
+                    # print(f"probs shape: {probs.shape}")
+                    # print(f"cutoffs: {cutoffs}")
                     hyps.append(probs[-1, :].detach().cpu().numpy())
                     refs.append(labels.detach().cpu().numpy())
+                    cutoff_times = ['2d','5d','13d','noDS']
+                    for time in cutoff_times:
+                        if cutoffs[time] != -1:
+                            hyps_temp[time].append(probs[cutoffs[time][0], :].detach().cpu().numpy())
+                            refs_temp[time].append(labels.detach().cpu().numpy())
 
+                    # print(f"ccutoffs: {cutoffs}, hyprs_temp: {hyps_temp}")
                     if ((t + 1) % grad_accumulation_steps == 0) or (
                         t + 1 == len(training_generator)
                     ):
@@ -100,17 +111,16 @@ class Trainer:
                 #     break
             print("Starting evaluation...")
             print("Epoch: %d" % (training_args['TOTAL_COMPLETED_EPOCHS']))
-            
             result = self.evaluate_and_save_results(
-                hyps, refs, mymetrics, training_args, validation_generator
+                hyps, refs, hyps_temp, refs_temp, mymetrics, training_args, validation_generator
             )
 
             training_args['CURRENT_PATIENCE_COUNT'] += 1
             training_args['TOTAL_COMPLETED_EPOCHS'] += 1
 
             if result["validation_f1_micro"] > training_args['CURRENT_BEST']:
-                CURRENT_BEST = result['validation_f1_micro']
-                CURRENT_PATIENCE_COUNT = 0
+                training_args['CURRENT_BEST'] = result['validation_f1_micro']
+                training_args['CURRENT_PATIENCE_COUNT'] = 0
                 best_path = os.path.join(self.config['project_path'], f"results/BEST_{self.config['run_name']}.pth")
                 if self.config["save_model"]:
                     self.save_model(result, training_args, best_path)
@@ -148,22 +158,15 @@ class Trainer:
                 },
                 save_path,
             )
-    
-    def evaluate_and_save_results(self, hyps, refs, mymetrics, training_args, validation_generator):
-        train_metrics = mymetrics.from_numpy(np.asarray(hyps), np.asarray(refs))
-        print(f"Calculating validation metrics with a val dataset of {len(validation_generator)}...")
-        validation_metrics = evaluate(
-            mymetrics, self.model, validation_generator, self.device, optimise_threshold=True
-        )
-
+    def save_results(self, train_metrics, validation_metrics, training_args, timeframe='all'):
         a = {
             f"validation_{key}": validation_metrics[key]
             for key in validation_metrics.keys()
         }
         b = {f"train_{key}": train_metrics[key] for key in train_metrics.keys()}
         result = {**a, **b}
-        print(result)
-        self.model.train()  # put model to training mode
+
+        #print(result)
 
         print(
             {
@@ -177,9 +180,30 @@ class Trainer:
         result_list = {k: [v] for k, v in result.items()}
         df = pd.DataFrame.from_dict(result_list)  # convert to datframe
 
-        results_path = os.path.join(self.config['project_path'], f"results/{self.config['run_name']}.csv")
+        results_path = os.path.join(self.config['project_path'], f"results/{self.config['run_name']}_{timeframe}.csv")
         results_df = pd.read_csv(results_path)
         results_df = pd.concat((results_df, df), axis=0, ignore_index=True)
         results_df.to_csv(results_path)  # update results
+
+        return result
+
+    def evaluate_and_save_results(self, hyps, refs, hyps_temp, refs_temp, mymetrics, training_args, validation_generator):
+        train_metrics = mymetrics.from_numpy(np.asarray(hyps), np.asarray(refs))
+        cutoff_times = ['2d', '5d', '13d', 'noDS']
+        train_metrics_temp = {time: mymetrics.from_numpy(np.asarray(hyps_temp[time]), np.asarray(refs_temp[time])) for time in cutoff_times}
+        #print(train_metrics_temp)
+        print(f"Calculating validation metrics with a val dataset of {len(validation_generator)}...")
+        validation_metrics, validation_metrics_temp = evaluate(
+            mymetrics, self.model, validation_generator, self.device, optimise_threshold=True
+        )
+        #print(validation_metrics_temp)
+        result = self.save_results(train_metrics, validation_metrics, training_args, timeframe='all')
+        print(result)
+        for time in cutoff_times:
+            _ = self.save_results(train_metrics_temp[time], validation_metrics_temp[time], training_args, timeframe=time)
+
+
+        self.model.train()  # put model to training mode
+
 
         return result
