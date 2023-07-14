cbb3598b1736e4071d3cccbbc639c0a4dd3c4822
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-07-14 09:36:44 +0100

    evaluate also auxiliary task

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -14,16 +14,18 @@ def evaluate(
     pred_cutoff=0.5,
     evaluate_temporal=False,
     optimise_threshold=False,
+    num_categories=1,
+    is_baseline=False,
 ):
     model.eval()
     with torch.no_grad():
         ids = []
-        hyps = []
-        refs = []
+        preds = {"hyps": [], "refs": [], "hyps_aux": [], "refs_aux": []}
+
         if evaluate_temporal:
-            hyps_temp ={'2d':[],'5d':[],'13d':[],'noDS':[]}
-            refs_temp ={'2d':[],'5d':[],'13d':[],'noDS':[]}
-          
+            preds["hyps_temp"] ={'2d':[],'5d':[],'13d':[],'noDS':[]}
+            preds["refs_temp"] ={'2d':[],'5d':[],'13d':[],'noDS':[]}
+ 
         avail_doc_count = []
         print(f"Starting validation loop...")
         for t, data in enumerate(tqdm(generator)):
@@ -43,32 +45,60 @@ def evaluate(
                 seq_ids=seq_ids.to(device, dtype=torch.long),
                 category_ids=category_ids.to(device, dtype=torch.long),
                 # note_end_chunk_ids=note_end_chunk_ids,
-            )
+            )    
+            if len(category_ids) > 1 and pred_categories is not None:
+                true_categories = F.one_hot(
+                    torch.concat([category_ids[1:], torch.tensor([num_categories])]),
+                    num_classes=num_categories+1
+                )
+
+                preds["hyps_aux"].append(pred_categories.detach().cpu().numpy())
+                preds["refs_aux"].append(true_categories.detach().cpu().numpy())
+            
             probs = F.sigmoid(scores)
             ids.append(data["hadm_id"][0].item())
             avail_doc_count.append(avail_docs)
-            hyps.append(probs[-1, :].detach().cpu().numpy())
-            refs.append(labels.detach().cpu().numpy())
+            preds["hyps"].append(probs[-1, :].detach().cpu().numpy())
+            preds["refs"].append(labels.detach().cpu().numpy())
             if evaluate_temporal:
                 cutoff_times = ['2d','5d','13d','noDS']
                 for time in cutoff_times:
                     if cutoffs[time] != -1:
-                        hyps_temp[time].append(probs[cutoffs[time][0], :].detach().cpu().numpy())
-                        refs_temp[time].append(labels.detach().cpu().numpy())
+                        preds["hyps_temp"][time].append(probs[cutoffs[time][0], :].detach().cpu().numpy())
+                        preds["refs_temp"][time].append(labels.detach().cpu().numpy())
 
         if optimise_threshold:
             pred_cutoff = mymetrics.get_optimal_microf1_threshold_v2(
-                np.asarray(hyps), np.asarray(refs)
+                np.asarray(preds["hyps"]), np.asarray(preds["refs"])
             )
+        else:
+            pred_cutoff = 0.5
 
-        computed_results = mymetrics.from_numpy(
-            np.asarray(hyps), np.asarray(refs), pred_cutoff=pred_cutoff
+
+        val_metrics = mymetrics.from_numpy(
+            np.asarray(preds["hyps"]),
+            np.asarray(preds["refs"]),
+            pred_cutoff=pred_cutoff
         )
-        val_metrics = mymetrics.from_numpy(np.asarray(hyps), np.asarray(refs))
+        if not is_baseline:
+            val_metrics_aux = mymetrics.from_numpy(
+                np.concatenate(preds["hyps_aux"]),
+                np.concatenate(preds["refs_aux"]),
+                pred_cutoff=pred_cutoff
+            )
+        else:
+            val_metrics_aux = None
+
         if evaluate_temporal:
             cutoff_times = ['2d', '5d', '13d', 'noDS']
-            val_metrics_temp = {time: mymetrics.from_numpy(np.asarray(hyps_temp[time]), np.asarray(refs_temp[time])) for time in cutoff_times}
+            val_metrics_temp = {
+                time: mymetrics.from_numpy(
+                    np.asarray(preds["hyps_temp"][time]),
+                    np.asarray(preds["refs_temp"][time]),
+                    pred_cutoff=pred_cutoff
+                ) for time in cutoff_times
+            }
         else:
             val_metrics_temp = None
   
-    return val_metrics, val_metrics_temp
+    return val_metrics, val_metrics_temp, val_metrics_aux
