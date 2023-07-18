443f7caea61348f6780661bb7f18f16705c0c282
mireiahernandez <mireiahernandezcaralt@outlook.es>
2023-07-18 18:20:37 +0100

    refactored evaluate so that it keeps old functionalities

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -17,6 +17,7 @@ def evaluate(
     num_categories=1,
     is_baseline=False,
     aux_task=None,
+    setup='latest',
 ):
     model.eval()
     with torch.no_grad():
@@ -32,41 +33,61 @@ def evaluate(
         for t, data in enumerate(tqdm(generator)):
             # TODO: fix code so that sequence ids embeddings can be used
             # right now they cannot be used
-            labels = data["label"][0][: model.num_labels]
-            input_ids = data["input_ids"][0]
-            attention_mask = data["attention_mask"][0]
-            seq_ids = data["seq_ids"][0]
-            category_ids = data["category_ids"][0]
-            avail_docs = seq_ids.max().item() + 1
-            # note_end_chunk_ids = data["note_end_chunk_ids"]
-            cutoffs = data["cutoffs"]
+            if setup == "random":
+                labels = data["label"][0][: model.num_labels]
+                input_ids = data["input_ids"][0]
+                attention_mask = data["attention_mask"][0]
+                seq_ids = data["seq_ids"][0]
+                category_ids = data["category_ids"][0]
+                avail_docs = seq_ids.max().item() + 1
+                # note_end_chunk_ids = data["note_end_chunk_ids"]
+                cutoffs = data["cutoffs"]
 
-            complete_sequence_output = []
-            # run through data in chunks of max_chunks
-            for i in range(0, input_ids.shape[0], model.max_chunks):
-                # only get the document embeddings
-                sequence_output = model(
-                    input_ids=input_ids[i : i + model.max_chunks].to(
-                        device, dtype=torch.long
-                    ),
-                    attention_mask=attention_mask[i : i + model.max_chunks].to(
-                        device, dtype=torch.long
-                    ),
-                    seq_ids=seq_ids[i : i + model.max_chunks].to(
-                        device, dtype=torch.long
-                    ),
-                    category_ids=category_ids[i : i + model.max_chunks].to(
-                        device, dtype=torch.long
-                    ),
-                    is_evaluation=True,
+                complete_sequence_output = []
+                # run through data in chunks of max_chunks
+                for i in range(0, input_ids.shape[0], model.max_chunks):
+                    # only get the document embeddings
+                    sequence_output = model(
+                        input_ids=input_ids[i : i + model.max_chunks].to(
+                            device, dtype=torch.long
+                        ),
+                        attention_mask=attention_mask[i : i + model.max_chunks].to(
+                            device, dtype=torch.long
+                        ),
+                        seq_ids=seq_ids[i : i + model.max_chunks].to(
+                            device, dtype=torch.long
+                        ),
+                        category_ids=category_ids[i : i + model.max_chunks].to(
+                            device, dtype=torch.long
+                        ),
+                        cutoffs=None,
+                        is_evaluation=True,
+                        # note_end_chunk_ids=note_end_chunk_ids,
+                    )
+                    complete_sequence_output.append(sequence_output)
+                # concatenate the sequence output
+                sequence_output = torch.cat(complete_sequence_output, dim=0)
+
+                # run through LWAN to get the scores
+                scores = model.label_attn(sequence_output, cutoffs=cutoffs)
+            else:
+                labels = data["label"][0][: model.num_labels]
+                input_ids = data["input_ids"][0]
+                attention_mask = data["attention_mask"][0]
+                seq_ids = data["seq_ids"][0]
+                category_ids = data["category_ids"][0]
+                avail_docs = seq_ids.max().item() + 1
+                # note_end_chunk_ids = data["note_end_chunk_ids"]
+                cutoffs = data["cutoffs"]
+
+                scores, _, aux_predictions = model(
+                    input_ids=input_ids.to(device, dtype=torch.long),
+                    attention_mask=attention_mask.to(device, dtype=torch.long),
+                    seq_ids=seq_ids.to(device, dtype=torch.long),
+                    category_ids=category_ids.to(device, dtype=torch.long),
+                    cutoffs = cutoffs,
                     # note_end_chunk_ids=note_end_chunk_ids,
                 )
-                complete_sequence_output.append(sequence_output)
-            # concatenate the sequence output
-            sequence_output = torch.cat(complete_sequence_output, dim=0)
-
-            # run through LWAN to get the scores
-            scores = model.label_attn(sequence_output)
             if aux_task == "next_document_category":
                 if len(category_ids) > 1 and aux_predictions is not None:
                     true_categories = F.one_hot(
@@ -83,10 +104,10 @@ def evaluate(
             preds["refs"].append(labels.detach().cpu().numpy())
             if evaluate_temporal:
                 cutoff_times = ["2d", "5d", "13d", "noDS"]
-                for time in cutoff_times:
-                    if cutoffs[time] != -1:
+                for n, time in enumerate(cutoff_times):
+                    if cutoffs[time][0] != -1:
                         preds["hyps_temp"][time].append(
-                            probs[cutoffs[time][0], :].detach().cpu().numpy()
+                            probs[n, :].detach().cpu().numpy()
                         )
                         preds["refs_temp"][time].append(labels.detach().cpu().numpy())
 
